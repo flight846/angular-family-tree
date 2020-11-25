54708b0f7feb8a3e36073056858b8a613babbdff
Yazid Ismail <flight846@gmail.com>
2020-11-25 23:52:51 +0800

    Patch childOf id

--- src/app/family/family.component.ts
+++ src/app/family/family.component.ts
@@ -9,93 +9,138 @@ import * as _ from 'lodash';
 export class FamilyComponent implements OnInit {
   members$ = [
     {
-      id: 1,
-      childOf: 0,
-      name: "Michael Lim",
-      relationship: "Father",
-      relationshipType: "elderGeneration",
-      relationshipStrength: "strong",
+      "type": "family",
+      "_id": "5faabdee364a1d24f49dd6a6",
+      "name": "Michael Lim",
+      "relationship": "father",
+      "relationshipStrength": "normal",
+      "birthYear": 1961,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "relationshipType": "elderGeneration",
+      "marriedTo": "Agnes Neo",
+      "childOf": "0",
+      "id": "5faabdee364a1d24f49dd6a6"
     },
     {
-      id: 9,
-      childOf: "Michael Lim",
-      name: "Desiree Lim",
-      relationship: "Sister",
-      relationshipType: "myGeneration",
-      relationshipStrength: "strong",
+      "type": "family",
+      "_id": "5faabe48364a1d24f49dd6a8",
+      "name": "Agnes Neo",
+      "relationship": "mother",
+      "relationshipStrength": "normal",
+      "birthYear": 1965,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "relationshipType": "elderGeneration",
+      "marriedTo": "Michael Lim",
+      "childOf": "0",
+      "id": "5faabe48364a1d24f49dd6a8"
     },
     {
-      id: 10,
-      childOf: 1,
-      name: "Jackson Lim",
-      relationship: "Brother",
-      relationshipType: "myGeneration",
-      relationshipStrength: "strong",
+      "type": "family",
+      "_id": "5faabeda364a1d24f49dd6aa",
+      "name": "Debra Lim",
+      "relationship": "sister",
+      "relationshipStrength": "strong",
+      "birthYear": 1990,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "relationshipType": "myGeneration",
+      "childOf": "5faabdee364a1d24f49dd6a6",
+      "id": "5faabeda364a1d24f49dd6aa"
     },
     {
-      id: 2,
-      childOf: 1,
-      name: "Me",
-      relationship: "me",
-      relationshipType: "myGeneration",
-      relationshipStrength: "strong",
+      "type": "family",
+      "_id": "5faabf0e364a1d24f49dd6ab",
+      "name": "Nelson Lim",
+      "relationship": "brother",
+      "relationshipStrength": "strong",
+      "birthYear": 1992,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "relationshipType": "myGeneration",
+      "childOf": "5faabdee364a1d24f49dd6a6",
+      "id": "5faabf0e364a1d24f49dd6ab"
     },
     {
-      id: 3,
-      childOf: 1,
-      name: "Jasseline Lim",
-      relationship: "Wife",
-      relationshipType: "myGeneration",
-      relationshipStrength: "strong",
+      "type": "nonFamily",
+      "_id": "5faacd5636ca362f7d4410b8",
+      "name": "Ahmad Driver",
+      "relationship": "eldersOthersMale",
+      "relationshipStrength": "normal",
+      "birthYear": 1965,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "childOf": "0",
+      "id": "5faacd5636ca362f7d4410b8"
     },
     {
-      id: 4,
-      childOf: 2,
-      name: "Gilbert Tan",
-      relationship: "Son",
-      relationshipType: "",
-      relationshipStrength: "strong",
+      "type": "family",
+      "_id": "5fb29f03336f770fd651ba89",
+      "name": "Lois Phua",
+      "relationship": "wife",
+      "relationshipType": "myGeneration",
+      "relationshipStrength": "strong",
+      "birthYear": 1994,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "marriedTo": "Test Teams",
+      "childOf": "5faabdee364a1d24f49dd6a6",
+      "id": "5fb29f03336f770fd651ba89"
     },
     {
-      id: 5,
-      childOf: 0,
-      name: "Angeline Neo",
-      relationship: "Mother",
-      relationshipType: "elderGeneration",
-      relationshipStrength: "strong",
+      "type": "family",
+      "_id": "5fb29f37336f770fd651ba8b",
+      "name": "Amos Lim",
+      "relationship": "son",
+      "relationshipType": "secondGeneration",
+      "relationshipStrength": "normal",
+      "birthYear": 2018,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "childOf": "5fbe7bdb502667201e42b687",
+      "id": "5fb29f37336f770fd651ba8b"
     },
     {
-      id: 6,
-      childOf: 0,
-      name: "Harold Lim",
-      relationship: "Uncle",
-      relationshipType: "elderGeneration",
-      relationshipStrength: "strong",
+      "type": "family",
+      "_id": "5fb2a2f5336f770fd651ba8d",
+      "name": "Bernies Lim",
+      "relationship": "daughter",
+      "relationshipType": "secondGeneration",
+      "relationshipStrength": "normal",
+      "birthYear": 2010,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "childOf": "5fbe7bdb502667201e42b687",
+      "id": "5fb2a2f5336f770fd651ba8d"
     },
     {
-      id: 7,
-      childOf: 4,
-      name: "Francis Tan",
-      relationship: "Grandson",
-      relationshipType: "",
-      relationshipStrength: "strong",
+      "type": "family",
+      "_id": "5fb2a430336f770fd651ba8f",
+      "name": "Ann Loh",
+      "relationship": "grandDaughter",
+      "relationshipType": "thirdGeneration",
+      "relationshipStrength": "weak",
+      "birthYear": 2010,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "__v": 0,
+      "childOf": "5fb29f37336f770fd651ba8b",
+      "isDeceased": true,
+      "id": "5fb2a430336f770fd651ba8f"
     },
     {
-      id: 8,
-      childOf: 2,
-      name: "Lissy Tan",
-      relationship: "Daughter",
-      relationshipType: "",
-      relationshipStrength: "strong",
-    },
-    {
-      id: 9,
-      childOf: 4,
-      name: "William Tan",
-      relationship: "Grandson",
-      relationshipType: "",
-      relationshipStrength: "strong",
-    },
+      "type": "family",
+      "_id": "5fbe7bdb502667201e42b687",
+      "name": "Test Teams",
+      "relationship": "me",
+      "relationshipType": "myGeneration",
+      "relationshipStrength": "strong",
+      "birthYear": 1992,
+      "familyId": "5faab7d7364a1d24f49dd6a4",
+      "childOf": "5faabdee364a1d24f49dd6a6",
+      "__v": 0,
+      "id": "5fbe7bdb502667201e42b687"
+    }
   ];
 
   currentMembers = [];
@@ -112,14 +157,14 @@ export class FamilyComponent implements OnInit {
 
   unflatten(array, parent?, tree?) {
     tree = typeof tree !== "undefined" ? tree : [];
-    parent = typeof parent !== "undefined" ? parent : { id: 0 };
+    parent = typeof parent !== "undefined" ? parent : { id: '0' };
 
     const children = _.filter(array, function (child) {
       return child.childOf === parent.id;
     });
 
     if (!_.isEmpty(children)) {
-      if (parent.id === 0) {
+      if (parent.id === '0') {
         tree = children;
       } else {
         parent["children"] = children;
